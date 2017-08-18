6e78b2af6900c4bb794309bf0aa1e1e5eca9666f
Anvaygoel <league@WTS5.attlocal.net>
2017-08-18 14:48:54 -0700

    day 5 (last day)

---
+++ src/day5/Robotgrafiti.java
@@ -0,0 +1,34 @@
+package day5;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Robotgrafiti {
+	public static void main(String[] args) {
+		Robot c3po = new Robot("batman");
+		
+c3po.setSpeed(10);
+c3po.penDown();
+c3po.setPenWidth(5);
+c3po.move(90);
+c3po.turn(45);
+c3po.move(45);
+c3po.turn(45);
+c3po.move(90);
+c3po.turn(45);
+c3po.move(45);
+c3po.turn(45);
+c3po.move(90);
+c3po.turn(180);
+c3po.move(45);
+c3po.turn(270);
+c3po.move(150);
+c3po.turn(180);
+c3po.move(250);
+
+
+	
+	
+	
+	}
+
+}
